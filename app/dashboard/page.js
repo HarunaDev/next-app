8ce11c716d981402aa939582131343f9b0834d7d
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-15 05:37:19 +0100

    created dashboard route with page

---
+++ app/dashboard/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Dashboard page</h1>
+      <Link href="/">Go to home</Link>
+    </div>
+  )
+}
+
+export default page
