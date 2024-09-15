11c4b137d3eb0c3de5d1238f463fc342a140cf1e
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-15 06:49:36 +0100

    rended image in /images route

---
+++ app/images/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Img from '@/assets/Food-Mock.png'
+import Image from 'next/image'
+
+const page = () => {
+  return (
+    <div>
+        <h1>Images will be displayed here</h1>
+        <Image src={Img} alt="Design Image" />
+    </div>
+  )
+}
+
+export default page
