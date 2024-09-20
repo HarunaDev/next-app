a44df43b8d94f8a949fd8c41bb7b08c4fb5380fa
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-20 03:36:00 +0100

    created todo component and pasted table row with dummy test data and delete button

---
+++ app/components/Todo.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <>
+    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                1
+              </th>
+              <td className="px-6 py-4">Egusi Soup</td>
+              <td className="px-6 py-4">Egusi seed, Bitter Leaf, Nziza Seed, Salt, Maggi, Fish, Black pepper</td>
+              <td className="px-6 py-4">Pending</td>
+              <td className="px-6 py-4">
+                <button className='py-2 px-4 bg-red-500 text-white rounded-md'>Delete</button>
+              </td>
+            </tr>
+    </>
+  )
+}
+
+export default Todo
