5567d16710dc2e6233474e0e0291a8e6e54004ea
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-20 03:59:50 +0100

    added and styled buttons

--- app/components/Todo.tsx
+++ app/components/Todo.tsx
@@ -14,8 +14,9 @@ const Todo = () => {
               <td className="px-6 py-4">Egusi Soup</td>
               <td className="px-6 py-4">Egusi seed, Bitter Leaf, Nziza Seed, Salt, Maggi, Fish, Black pepper</td>
               <td className="px-6 py-4">Pending</td>
-              <td className="px-6 py-4">
+              <td className="px-6 py-4 flex items-center justify-center gap-1">
                 <button className='py-2 px-4 bg-red-500 text-white rounded-md'>Delete</button>
+                <button className='py-2 px-4 bg-green-500 text-white rounded-md'>Done</button>
               </td>
             </tr>
     </>
